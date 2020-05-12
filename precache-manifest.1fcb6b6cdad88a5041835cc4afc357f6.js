934a83c86b763d21958e41fad2b19cdeadab422d
makanjuolakunle@gmail.com <makanjuolakunle@gmail.com>
2020-05-12 04:49:56 +0100

    Updates

---
+++ precache-manifest.1fcb6b6cdad88a5041835cc4afc357f6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c75b6b4f58fc98331f9ad305ff47863f",
+    "url": "/monster-rolodex/index.html"
+  },
+  {
+    "revision": "56f861e54d6d6f00f73a",
+    "url": "/monster-rolodex/static/css/main.61e80bf4.chunk.css"
+  },
+  {
+    "revision": "7c9de054972a4d398550",
+    "url": "/monster-rolodex/static/js/2.8080748b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monster-rolodex/static/js/2.8080748b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "56f861e54d6d6f00f73a",
+    "url": "/monster-rolodex/static/js/main.558589b2.chunk.js"
+  },
+  {
+    "revision": "b446102fc910fc3f6b8a",
+    "url": "/monster-rolodex/static/js/runtime-main.f915b6d4.js"
+  }
+]);
